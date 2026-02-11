f70a8a2edf1bd5e401e375efb8b74e52d2e9f086
nino <pilopilosimon1990@gmail.com>
2026-02-11 16:11:35 +0100

    Update and rename twrp_P661N.mk to pb_P661N.mk

--- pb_P661N.mk
+++ pb_P661N.mk
@@ -16,13 +16,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Inherit some common TWRP stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from P661N device
 $(call inherit-product, device/itel/P661N/device.mk)
 
 PRODUCT_DEVICE := P661N
-PRODUCT_NAME := twrp_P661N
+PRODUCT_NAME := pb_P661N
 PRODUCT_BRAND := Itel
 PRODUCT_MODEL := itel P55 5G
 PRODUCT_MANUFACTURER := ITEL
