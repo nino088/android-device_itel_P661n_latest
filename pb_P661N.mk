2d628b88111b8b17ab84ecaa1cd020ba0a968d0c
nino <pilopilosimon1990@gmail.com>
2026-02-11 20:45:06 +0100

    Update pb_P661N.mk

--- pb_P661N.mk
+++ pb_P661N.mk
@@ -1,31 +1,22 @@
 #
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2022 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
+# Inherit from P661N device
+$(call inherit-product, device/itel/P661n/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
-# Enable project quotas and casefolding for emulated storage without sdcardfs
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/pb/config/common.mk)
 
-# Inherit from P661N device
-$(call inherit-product, device/itel/P661N/device.mk)
-
 PRODUCT_DEVICE := P661N
 PRODUCT_NAME := pb_P661N
-PRODUCT_BRAND := Itel
-PRODUCT_MODEL := itel P55 5G
-PRODUCT_MANUFACTURER := ITEL
+PRODUCT_BRAND := ITEL
+PRODUCT_MODEL := itel P661N
+PRODUCT_MANUFACTURER := itel
 
 PRODUCT_GMS_CLIENTID_BASE := android-transsion
 
