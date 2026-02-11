45717f7004611d1e5b7e3e40353236345998bd6c
nino <pilopilosimon1990@gmail.com>
2026-02-11 17:58:48 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -195,7 +195,3 @@ TW_LOAD_VENDOR_BOOT_MODULES := true
 # Version
 TW_DEVICE_VERSION := nino
 TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
-
-# PBRP
-PB_TORCH_PATH := /sys/devices/virtual/torch/torch/torch_level
-PB_TORCH_MAX_BRIGHTNESS := 1
