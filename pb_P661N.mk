74c8493dbf412fb64e936bae141e181891f4c29b
nino <pilopilosimon1990@gmail.com>
2026-02-11 21:48:31 +0100

    Update pb_P661N.mk

--- pb_P661N.mk
+++ pb_P661N.mk
@@ -5,7 +5,7 @@
 #
 
 # Inherit from P661N device
-$(call inherit-product, device/itel/P661n/device.mk)
+$(call inherit-product, device/itel/P661N/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
