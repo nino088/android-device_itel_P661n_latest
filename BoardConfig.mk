078e3626d20e2935b223bb03f3ea2698398327fc
nino <pilopilosimon1990@gmail.com>
2026-02-13 21:03:32 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -195,5 +195,5 @@ TW_DEVICE_VERSION := nino
 TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
 
 # PB Torch
-PB_TORCH_PATH := "/sys/devices/virtual/torch/torch/torch_level"
+PB_TORCH_PATH := "/sys/class/torch/torch/torch_level"
 PB_TORCH_MAX_BRIGHTNESS := 1
