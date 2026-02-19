2a855d19f971cb6a640b950e25ebc931b872b39e
nino <pilopilosimon1990@gmail.com>
2026-02-19 09:45:33 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -178,7 +178,8 @@ TW_CUSTOM_CPU_POS := 270
 # FastbootD
 TW_INCLUDE_FASTBOOTD := true
 
-# Storage
+# Storage / OTG
+TW_HAS_OTG := true
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.usb0/lun.%d/file
 TW_EXCLUDE_DEFAULT_USB_INIT := true
