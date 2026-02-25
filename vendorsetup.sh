b1dd77a17245964b43188bb5cc84856eee120550
nino <pilopilosimon1990@gmail.com>
2026-02-26 00:51:53 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,73 +1,56 @@
 #!/bin/bash
-#
-#	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2020-2025 The OrangeFox Recovery Project
-#
-#	OrangeFox is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	any later version.
-#
-#	OrangeFox is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
-#
-# 	This software is released under GPL version 3 or any later version.
-#	See <http://www.gnu.org/licenses/>.
-#
-# 	Please maintain this if you use this script or any part of it
-#
 
-    export LC_ALL="C.UTF-8"
-    export ALLOW_MISSING_DEPENDENCIES=true
+export OF_DISABLE_OTA_MENU=1
+export FOX_AB_DEVICE=1
+export FOX_VIRTUAL_AB_DEVICE=1
+export OF_DEFAULT_KEYMASTER_VERSION=4.1
+export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+export OF_MAINTAINER="nino"
+export FOX_VARIANT="A13+"
+export OF_FLASHLIGHT_ENABLE=0
 
-#OFR build settings & info
-    export TARGET_DEVICE_ALT="P661N"
-    export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
-    export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-    export FOX_VENDOR_BOOT_RECOVERY_FULL_REFLASH=1
-    export FOX_VENDOR_BOOT_RECOVERY=1
-    export FOX_DELETE_MAGISK_ADDON=1
-    export FOX_DELETE_AROMAFM=1
-    export FOX_ENABLE_APP_MANAGER=1
-    export FOX_SETTINGS_ROOT_DIRECTORY=/persist/OFRP
-    export FOX_RESET_SETTINGS=1
-    export OF_DEFAULT_KEYMASTER_VERSION=4.1
-    export FOX_BUILD_TYPE="Stable"
-
-    # lz4 compression
-	export OF_USE_LZ4_COMPRESSION=1
-
-    # build all the partition tools
-	export OF_ENABLE_ALL_PARTITION_TOOLS=1
-    
-    #OFR binary files
-    export FOX_USE_BASH_SHELL=1
-    export FOX_USE_NANO_EDITOR=1
-    export FOX_USE_TAR_BINARY=1
-    export FOX_USE_SED_BINARY=1
-    export FOX_USE_XZ_UTILS=1
-    export FOX_ASH_IS_BASH=1
-    export OF_ENABLE_LPTOOLS=1
+# KernelSU / SukiSu support
+export FOX_ENABLE_KERNELSU_SUPPORT=1
+export FOX_ENABLE_KERNELSU_NEXT_SUPPORT=1
+export FOX_ENABLE_SUKISU_SUPPORT=1
  
-   # don't spam the console with loop errors
-	export OF_LOOP_DEVICE_ERRORS_TO_LOG=1
-	
-    #OTA
-    export FOX_AB_DEVICE=1
-    export FOX_VIRTUAL_AB_DEVICE=1
-    export OF_SUPPORT_VBMETA_AVB2_PATCHING=1
+export FOX_USE_BASH_SHELL=1
+export FOX_USE_NANO_EDITOR=1
+export FOX_USE_TAR_BINARY=1
+export FOX_USE_SED_BINARY=1
+export FOX_USE_XZ_UTILS=1
+export FOX_ASH_IS_BASH=1
+export OF_ENABLE_LPTOOLS=1
+export FOX_DELETE_MAGISK_ADDON=1
+export FOX_DELETE_AROMAFM=1
+export FOX_ENABLE_APP_MANAGER=1
+export OF_SUPPORT_VBMETA_AVB2_PATCHING=1
 
-	 # KernelSU / SukiSu support
-   export FOX_ENABLE_KERNELSU_SUPPORT=1
-   export FOX_ENABLE_KERNELSU_NEXT_SUPPORT=1
-   export FOX_ENABLE_SUKISU_SUPPORT=1
- 
-    # Haptics patch - simple apply, no checks
-    SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-    PATCH_FILE="$SCRIPT_DIR/patches/0001-Add-regulator-vibrator-haptics-support.patch"
-    WORKSPACE_ROOT="$(cd "$SCRIPT_DIR/../../.." && pwd)"
+export FOX_USE_DATA_RECOVERY_FOR_SETTINGS=1
 
-    cd "$WORKSPACE_ROOT"
-    git apply "$PATCH_FILE" 2>/dev/null || true
+export OF_LOOP_DEVICE_ERRORS_TO_LOG=1
+
+export OF_USE_LZ4_COMPRESSION=true
+
+export OF_SCREEN_H=2400
+export OF_STATUS_H=95
+export OF_STATUS_INDENT_LEFT=48
+export OF_STATUS_INDENT_RIGHT=48
+export OF_ALLOW_DISABLE_NAVBAR=0
+export OF_CLOCK_POS=1
+
+export USE_CCACHE=1
+export CCACHE_EXEC=/usr/bin/ccache
+export CCACHE_MAXSIZE="5G"
+export CCACHE_DIR="~/ccache"
+
+if [ ! -d ${CCACHE_DIR} ];
+then
+  echo "CCACHE Directory/Partition is not mounted at \"${CCACHE_DIR}\""
+  echo "Please edit the CCACHE_DIR build variable or mount the directory."
+fi
+
+export LC_ALL="C"
+
+# Clone to fix build on minimal manifest
+git clone https://android.googlesource.com/platform/external/gflags/ -b android-12.1.0_r4 external/gflags
