ccab65a746085a823dc27551a78c171790e7c8fa
nino <pilopilosimon1990@gmail.com>
2026-03-04 23:29:29 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -187,7 +187,7 @@ RECOVERY_SDCARD_ON_DATA := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.usb0/lun.%d/file
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_USB_STORAGE := true
-TW_USB_OTG_STORAGE =: tru
+TW_USB_OTG_STORAGE =: true
 TW-USES_EXTERNAL_STOREAGE := true
 
 # Vendor Boot
