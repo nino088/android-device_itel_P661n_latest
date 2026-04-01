6a62b0075c11f823b057fe3f64df7ffda92e1927
nino <pilopilosimon1990@gmail.com>
2026-04-01 19:14:26 +0100

    Update and rename flashlight.sh to torch.sh

--- recovery/root/flashlight.sh
+++
@@ -1,3 +0,0 @@
-mkdir /flashlight
-ln -s /sys/devices/virtual/camera/flash/rear_flash /flashlight/brightness
-echo 1 > /flashlight/brightness
---
+++ recovery/root/torch.sh
@@ -0,0 +1,3 @@
+mkdir /torch
+ln -s /sys/class/torch/torch/torch_level /torch/torch_level
+echo 1 > /torch/torch_level
