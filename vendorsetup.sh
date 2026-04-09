eec174a476a05cfe2f5156bc37727ef9945f0b59
nino <pilopilosimon1990@gmail.com>
2026-04-09 21:11:54 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,6 @@
 git clone https://android.googlesource.com/platform/external/googletest/ -b /android-12.1.0_r4 external/googletest
+
+# Fix torch
+mkdir /torch
+ln -s /sys/class/torch/torch/torch_level /torch/torch_level
+echo 1 > /torch/torch_level
